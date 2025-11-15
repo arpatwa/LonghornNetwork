05b583ba05727ed91fef448fdda33c81ad362116
Anya Patwa <anyapatwa@utexas.edu>
2025-11-14 19:56:10 -0600

    Testing push to Anya-Branch

--- src/ChatThread.java
+++ src/ChatThread.java
@@ -1,4 +1,10 @@
 public class ChatThread implements Runnable {
+    /**
+     * This is the constructor for a ChatThread object
+     * @param sender is who sends a message
+     * @param receiver is who gets the message sent
+     * @param message is the message to be sent
+     */
     public ChatThread(UniversityStudent sender, UniversityStudent receiver, String message) {
         // Constructor
     }
